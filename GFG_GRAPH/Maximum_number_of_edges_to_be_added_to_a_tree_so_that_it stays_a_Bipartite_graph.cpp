8b55cd07516891b41a7428fc121f59aaa28355cc
ashu-oo7 <ashu0705000@gmail.com>
2022-10-03 00:21:04 +0530

    https://www.geeksforgeeks.org/maximum-number-edges-added-tree-stays-bipartite-graph/

---
+++ GFG_GRAPH/Maximum_number_of_edges_to_be_added_to_a_tree_so_that_it stays_a_Bipartite_graph.cpp
@@ -0,0 +1,37 @@
+#include<bits/stdc++.h>
+using namespace std;
+int c0,c1;
+vector<bool>visited;
+void dfs(vector<int>graph[],int node,int color){
+	visited[node] = true;
+	int ncolor;
+	if(color == 0){
+		ncolor = 1;
+		c0++;
+	}
+	else{
+		ncolor = 0;
+		c1++;
+	}
+	for(int i = 0;i<graph[node].size();i++){
+		if(!visited[graph[node][i]]){
+			dfs(graph,graph[node][i],ncolor);
+		}
+	}
+}
+int main(){
+	c0 = c1 = 0;
+	int n,m;
+	cin >> n >> m;
+	vector<int>graph[n];
+	visited = vector<bool>(n,false);
+	for(int i = 0;i<m;i++){
+		int a,b;
+		cin >> a >> b;
+		graph[a].push_back(b);
+		graph[b].push_back(a);
+	}
+	dfs(graph,0,0);
+	int ans = c0*c1 - (n-1);
+	cout<<ans<<endl;
+}\
